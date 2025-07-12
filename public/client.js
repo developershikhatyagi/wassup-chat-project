f6315a59ffdf3eae4bb87fddd62f48b80a318bf3
Shikha <shikhatyagi419@catalyseresearch.com>
2025-07-12 16:54:49 +0530

    wassup-chat-project

---
+++ public/client.js
@@ -0,0 +1,63 @@
+const socket = io()
+
+let name;
+
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message__area')
+
+do{
+    name = prompt('Please enter your name')
+} while(!name);
+
+textarea.addEventListener('keyup' , (e) => {
+    if(e.key === 'Enter') {
+        sendMessage(e.target.value)
+    }
+});
+
+
+
+function sendMessage(message) {
+    let msg = {
+        user: name,
+        message: message.trim()
+    }
+
+    //Append
+
+    appendMessage(msg, 'Outgoing')
+
+    
+// Send to server//
+
+ socket.emit('message', msg); 
+ textarea.value = '';
+}
+
+
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+
+    let className = type 
+
+    mainDiv.classList.add(className, 'message')
+
+
+    let markup = `
+    
+    <h4>${msg.user}</h4>
+    <p>${msg.message}</p>
+
+    `
+
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+}
+
+
+//Receieve Message//
+
+socket.on('message' , (msg) => {
+ appendMessage(msg, 'incoming')
+})
